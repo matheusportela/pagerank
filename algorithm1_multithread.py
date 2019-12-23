07040c6eca4046212f765396cb6a294303895430
Matheus Vieira Portela <matheus.v.portela@gmail.com>
2019-12-23 14:03:49 -0500

    Implement algorithm 1 multithreaded

---
+++ algorithm1_multithread.py
@@ -0,0 +1,95 @@
+import queue
+import threading
+import time
+
+import networkx as nx
+
+class Graph:
+    def __init__(self):
+        self.graph = nx.DiGraph()
+
+    def __repr__(self):
+        return str(self.graph.edges)
+
+    def load(self, filename):
+        with open(filename) as f:
+            for line in f:
+                source_node, destination_node = eval(line)
+                self.graph.add_edge(source_node, destination_node)
+
+    def calculate_pagerank(self, m=0.15):
+        start_channels = {n: queue.Queue() for n in self.graph.nodes}
+        end_channels = {n: queue.Queue() for n in self.graph.nodes}
+        data_channels = {n: queue.Queue() for n in self.graph.nodes}
+        nodes = []
+
+        for node in self.graph.nodes:
+            node = Node(
+                node_id=node,
+                neighbors=list(self.graph.neighbors(node)),
+                num_nodes=len(self.graph.nodes),
+                m=m,
+                start_channels=start_channels,
+                end_channels=end_channels,
+                data_channels=data_channels,
+            )
+            node.start()
+            nodes.append(node)
+
+        for _ in range(100):
+            for node in self.graph.nodes:
+                start_channels[node].put(None)
+                end_channels[node].get()
+
+        return {node.id: node.x for node in nodes}
+
+
+class Node(threading.Thread):
+    def __init__(self, node_id, neighbors, num_nodes, m, start_channels, end_channels, data_channels):
+        super().__init__(daemon=True)
+        self.id = node_id
+        self.neighbors = neighbors
+        self.m = m
+        self.n = len(self.neighbors)
+        self.x = self.m/num_nodes
+        self.z = self.m/num_nodes
+        self.start_channels = start_channels
+        self.end_channels = end_channels
+        self.data_channels = data_channels
+
+    def run(self):
+        while True:
+            self.start_channels[self.id].get()
+            self.run_pagerank_step()
+            self.end_channels[self.id].put(None)
+
+    def run_pagerank_step(self):
+        self.send_data()
+        self.update_pagerank()
+
+    def send_data(self):
+        for dst in self.neighbors:
+            self.data_channels[dst].put((self.id, self.n, self.z))
+
+    def update_pagerank(self):
+        x = self.x
+        z = 0
+
+        while not self.data_channels[self.id].empty():
+            src, nj, zj = self.data_channels[self.id].get()
+            x += ((1 - self.m)/nj)*zj
+            z += ((1 - self.m)/nj)*zj
+
+        self.x = x
+        self.z = z
+
+
+
+def main():
+    graph = Graph()
+    graph.load('graph.txt')
+    print(graph.calculate_pagerank())
+    print(nx.pagerank(graph.graph, alpha=0.85))
+
+if __name__ == '__main__':
+    main()
